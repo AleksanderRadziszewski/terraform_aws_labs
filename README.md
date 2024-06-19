f39f05eeb2f5412e08e79c6379ba56734c6ea90f
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2024-06-19 17:49:08 +0200

    README.md modified, website-module README.md scope changed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# terraform_aws_labs
+# Terraform AWS exercises
+
+1. Module: `public-modules` -> Using the vpc
+2. Module: `website-module` -> Using a local module to create a website in the S3 bucket
--- website-module/aws-s3-website/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.website_bucket.bucket
-}
-
-output "website_url" {
-  value = "http://${aws_s3_bucket.website_bucket.bucket}.s3-website-${var.aws_region}.amazonaws.com"
-}
